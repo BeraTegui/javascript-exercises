79891933f444ef4a39801a093796991c86918301
BeraTegui <manubased@gmail.com>
2025-01-12 09:42:05 -0300

    feat js exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(word) {
+        word = word.split('');
+        let newStr = '';
+        for(let i = (word.length - 1); i >= 0; i--){
+        newStr += word[i];
+        }
+        return newStr;
 
+        // Una solución más sencilla...
+        // return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
